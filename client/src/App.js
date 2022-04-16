7634ae38c83076db22442a8ac01d4a41f355e04c
Om Sharma <omsharma.theplayer11@gmail.com>
2022-04-16 11:55:31 +0530

    Changed the background image

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { AppBar, Typography } from "@mui/material";
 import VideoPlayer from "./components/VideoPlayer";
 import Notifications from "./components/Notifications"
 import Options from "./components/Options";
-
+import { makeStyles } from "@mui/material";
 
 const App = () => {
   return (
