b7460165aea6e3bee2fe28e3cb466330bc85222e
Om Sharma <omsharma.theplayer11@gmail.com>
2022-04-16 11:22:50 +0530

    Installed material-ui and working as expected

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { AppBar, Typography } from '@mui/material';
 const App = () => {
   return (
-    <div>Video chat theplayer</div>
+    // <div>Video chat theplayer</div>
+    <AppBar position='static' color='inherit' >
+        <Typography variant='h2' align='center'> Naughty boi </Typography>
+    </AppBar>
   )
 }
 
