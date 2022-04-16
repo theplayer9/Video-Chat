32591d988cca1621a7a4cda68f7f710b32996fab
Om Sharma <omsharma.theplayer11@gmail.com>
2022-04-16 14:45:23 +0530

    AppBar working fine.But it's width is not regulating

--- client/src/App.js
+++ client/src/App.js
@@ -3,42 +3,46 @@ import { AppBar, Typography } from "@mui/material";
 import VideoPlayer from "./components/VideoPlayer";
 import Notifications from "./components/Notifications";
 import Options from "./components/Options";
-import { makeStyles } from "@mui/styles";
+import { makeStyles } from  "@material-ui/core";
 
-// const useStyle = makeStyles((theme) => ({
-//   appBar: {
-//     borderRadius: 15,
-//     margin: "30px 100px",
-//     display: "flex",
-//     flexDirection: "row",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     width: "600px",
-//     border: "2px solid black",
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    height: '20%',
+    border: '2px solid black',
+    // marginLeft: '10px',
+    // marginRight: '150px',
 
-//     [theme.breakpoints.down("xs")]: {
-//       width: "90%",
-//     },
-//   },
-//   image: {
-//     marginLeft: "15px",
-//   },
-//   wrapper: {
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//     width: "100%",
-//   },
-// }));
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: "center",
+    width: '100%',
+    // justifyContent: 'center'
+  },
+}));
 
 const App = () => {
-
-    // const classes = useStyle()
+  const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.wrapper} >
       {/* <div>Video chat theplayer</div> */}
-      <AppBar  position="static" color="inherit">
+      <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography variant="h2" align="center">
           Naughty boi
         </Typography>
